9bd896dbf7b3812fd6466f13f01071244308d93f
Leegiwon <dlrldnjs73@naver.com>
2025-04-04 19:30:15 +0900

    SignUpSuccess Screen

--- LeadMe_front/LeadMeApp/App.tsx
+++ LeadMe_front/LeadMeApp/App.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignUpScreen from './src/screen/SignUpScreen';
 import LoginScreen from './src/screen/LoginScreen';
+import EditProfileScreen from './src/screen/EditProfileScreen';
+import SignUpSuccess from './src/screen/SignUpSuccess';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +14,8 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="EditProfile" component={EditProfileScreen} options = {{headerShown: false}} />
+        <Stack.Screen name="SignUpSuccess" component={SignUpSuccess} options = {{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
